878fdff137ac7f30933ef223faad64adcc1d8212
soorajc <soorajcmpm@gmail.com>
2016-08-04 19:33:50 +0530

    sound pause added

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,11 +1,11 @@
 
 var SinglePlayer = require('./singleplayer/singleplayer.js');
 var MultiPlayer = require('./multiplayer/multiplayer.js');
-
+var sound = new Audio("../sound/machines.mp3");
 
 var MainApp = React.createClass({
 
- getInitialState: function() {
+  getInitialState: function() {
    return {option : "home" };
  },
 
@@ -14,61 +14,66 @@ var MainApp = React.createClass({
  },
 
  componentDidMount() {
-   var snd = new Audio("../sound/machines.mp3");
-   snd.play();
+    sound.play();
  },
 
- render: function() {
-   var tileStyle = {
-     marginTop: 10
-   }
+  render: function() {
+    var tileStyle = {
+      marginTop:10
+    }
 
-   var singleButton = {
-     position: "absolute",
-     marginLeft: -850,
-     marginTop: 500,
-     marginBottom: 10,
-     fontSize: 20,
-   }
+    var singleButton = {
+      position: "absolute",
+      marginLeft:-850,
+      marginTop: 500,
+      marginBottom: 10,
+      fontSize: 20,
+    }
 
-   var multipleButton = {
-     position: "absolute",
-     marginLeft: -550,
-     marginTop: 500,
-     marginBottom: 10,
-     fontSize: 20,
-   }
+    var multipleButton = {
+      position: "absolute",
+      marginLeft:-550,
+      marginTop: 500,
+      marginBottom: 10,
+      fontSize: 20,
+    }
 
-   var setImage = {
-     paddingLeft: 5,
-     paddingTop: 20,
-     paddingRight: 5,
-     paddingBottom: 20,
-   }
+    var setImage = {
+      paddingLeft:5,
+      paddingTop:20,
+      paddingRight: 5,
+      paddingBottom: 20,
+    }
     return (
       <div className="col-md-12">
-        {(() => {
-        if (this.state.option == "home") {
-         return(
-           <div>
-             <div className="col-md-12" style={setImage}>
-                <img src="./images/background.png" height="100%" width="100%"/>
-             </div>
-              <button type="button" className="btn btn-success" onClick={this.handleMenu.bind(this,"single")} style={singleButton}>SinglePlayer</button>
-              <button type="button" className="btn btn-danger" onClick={this.handleMenu.bind(this,"multi")} style={multipleButton}>MultiPlayer</button>
+      {(() => {
+      if (this.state.option == "home") {
+        sound.play();
+       return(
+         <div>
+           <div className="col-md-12" style={setImage}>
+            <img src="./images/background.png" height="100%" width="100%"/>
            </div>
-         );
-        }else if (this.state.option == "single"){
-        return(
-          <SinglePlayer handleMenu={this.handleMenu.bind(this, "home")}/>
-        );
-        }else if (this.state.option == "multi"){
-        return(
-          <MultiPlayer handleMenu={this.handleMenu.bind(this, "home")}/>
-        );
-        }
-        })()}
-     </div>
+            <button type="button" className="btn btn-success" onClick={this.handleMenu.bind(this,"single")} style={singleButton}>SinglePlayer</button>
+            <button type="button" className="btn btn-danger" onClick={this.handleMenu.bind(this,"multi")} style={multipleButton}>MultiPlayer</button>
+         </div>
+       );
+      }
+      else if (this.state.option == "single"){
+        sound.pause();
+        sound.currentTime = 0;
+      return(
+        <SinglePlayer handleMenu={this.handleMenu.bind(this, "home")}/>
+      );
+    }else if (this.state.option == "multi"){
+      sound.pause();
+      sound.currentTime = 0;
+      return(
+        <MultiPlayer handleMenu={this.handleMenu.bind(this, "home")}/>
+      );
+    }
+      })()}
+      </div>
     );
   }
 });
